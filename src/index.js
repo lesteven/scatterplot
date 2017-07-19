6b22801c5e0e742b9f33148fe74d258a531f8edf
steven <stevenle2011@gmail.com>
2017-07-19 14:15:15 +0700

    fetch data and add scales

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
-import * as d3 from 'd3';
+//import * as d3 from 'd3';
+import {
+	selectAll,
+	select,
+	selection} from 'd3-selection';
+import {timeParse} from 'd3-time-format';
+import {scaleTime,scaleLinear} from 'd3-scale';
+import {range,extent,max} from 'd3-array';
+import {axisBottom,axisLeft} from 'd3-axis';
 require('./index.css');
 
+
+const url ='https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
+getData(url)
+
+
+function getData(url){
+	fetch(url)
+	.then(response=>response.json())
+	.then(data=>{
+		console.log(data)
+		drawGraph(data)
+	})
+}
+function drawGraph(data){
+	//variable holding svg attributes
+	const margin ={top:50,bottom:50,left:50,right:50}
+	const width = 950;
+	const height = 450;
+	const innerHeight = height - margin.top - margin.bottom;
+	const innerWidth = width - margin.left - margin.right;
+
+	//creates svg
+	let svg = select("body")
+		.append("svg")
+		.attr("width",width)
+		.attr("height",height)
+		.attr("class","graph")
+		.append('g').attr('transform','translate('
+			+ margin.left + ',' + margin.top + ')');
+
+	//parse date for x-axis
+	let parseTime = timeParse('%M:%S');
+
+
+	//set ranges
+	let xScale = scaleTime()
+		.range([0,innerWidth]);
+
+	let yScale = scaleLinear()
+		.range([innerHeight,0]);
+
+	//format data
+	
+	data.forEach(function(d){
+		d.Time = parseTime(d.Time)
+	})
+
+	//set domain
+	xScale.domain(extent(data,function(d){
+		//console.log(d.Time)
+		return d.Time
+	}))
+	yScale.domain(extent(data,function(d){
+		return d.Place
+	}))
+	
+	console.log(yScale.domain(),xScale.domain())
+}
